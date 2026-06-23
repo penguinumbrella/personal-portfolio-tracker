e9af1b576fa1f9218f6122a1cf12eb21ba14473a
onyxlurae <onyxlbennett@gmail.com>
2026-06-23 18:29:46 -0400

    merged queries and matched naming conventions

--- project2_personal_portfolio_tracker/schema.sql
+++ project2_personal_portfolio_tracker/schema.sql
@@ -7,11 +7,11 @@ CREATE TABLE IF NOT EXISTS portfolio.users (
 	password_hash	VARCHAR(64) NOT NULL
 );
 
-DROP TYPE IF EXISTS investment_type CASCADE;
-CREATE TYPE investment_type AS ENUM (
+DROP TYPE IF EXISTS portfolio.investment_type CASCADE;
+CREATE TYPE portfolio.investment_type AS ENUM (
 	'Brokerage',
-	'Traditional IRA',
-	'Roth IRA',
+	'Traditional_IRA',
+	'Roth_IRA',
 	'401(k)',
 	'HSA'
 );
@@ -19,8 +19,8 @@ CREATE TYPE investment_type AS ENUM (
 DROP TABLE IF EXISTS portfolio.investment_account;
 CREATE TABLE IF NOT EXISTS portfolio.investment_account (
 	id					BIGSERIAL 		PRIMARY KEY,
-	nickname	VARCHAR(64) 	NOT NULL UNIQUE,
-	account_type		investment_type	NOT NULL,
+	nickname			VARCHAR(64) 	NOT NULL UNIQUE,
+	account_type		portfolio.investment_type	NOT NULL,
 	institution_name	VARCHAR(64) 	NOT NULL,
 	date_opened			DATE 			DEFAULT CURRENT_DATE NOT NULL,
 	user_id 			BIGINT 			NOT NULL,
@@ -30,3 +30,55 @@ CREATE TABLE IF NOT EXISTS portfolio.investment_account (
 		REFERENCES portfolio.users(id)
 		ON DELETE CASCADE
 );
+
+DROP TYPE IF EXISTS portfolio.sector_type; 
+CREATE TYPE portfolio.sector_type AS ENUM (
+	'TECHNOLOGY',
+	'HEALTHCARE',
+	'FINANCIALS',
+	'CONSUMER',
+	'ENERGY',
+	'INDUSTRIALS',
+	'UTILITIES',
+	'REAL_ESTATE'
+);
+
+DROP TYPE IF EXISTS portfolio.security_type; 
+CREATE TYPE portfolio.security_type AS ENUM (
+	'STOCK', 
+	'ETF', 
+	'MUTUAL_FUND', 
+	'BOND');
+
+DROP TABLE IF EXISTS portfolio.security;
+CREATE TABLE IF NOT EXISTS portfolio.security (
+	id 				BIGSERIAL 		PRIMARY KEY,
+	ticker_symbol 	VARCHAR(10) 	NOT NULL,
+	name 			VARCHAR(64) 	NOT NULL,
+	sector 			portfolio.sector_type 	NOT NULL,
+	type 	 		portfolio.security_type 	NOT NULL,
+	general_notes 	TEXT
+);
+
+DROP TABLE IF EXISTS portfolio.holding;
+CREATE TABLE IF NOT EXISTS portfolio.holding (
+	account_id 		BIGINT NOT NULL,
+	security_id 	BIGINT NOT NULL,
+	PRIMARY KEY (account_id, security_id),
+	num_shares		INT		NOT NULL,
+	cost_per_share	INT		NOT NULL,
+	purchase_date	DATE 	NOT NULL,
+	CONSTRAINT 			holding_account
+		FOREIGN KEY 	(account_id) 
+		REFERENCES 		portfolio.investment_account (id) 
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	CONSTRAINT 			holding_security
+		FOREIGN KEY 	(security_id) 
+		REFERENCES 		portfolio.security (id) 
+		ON UPDATE CASCADE 
+		ON DELETE CASCADE
+);
+	
+CREATE INDEX IF NOT EXISTS holding_security_idx
+    ON portfolio.holding(security_id);
