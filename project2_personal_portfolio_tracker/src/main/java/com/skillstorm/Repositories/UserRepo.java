0f07b0774783f0061564ce0b63dc529a1da66a5a
Gina Peng <ginapeng2002@gmail.com>
2026-06-23 13:44:34 -0500

    feat: UserRepo

---
+++ project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Repositories/UserRepo.java
@@ -0,0 +1,9 @@
+package com.skillstorm.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skillstorm.Models.User;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+    
+}
