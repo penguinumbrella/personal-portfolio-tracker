e27f2c2d65c7536735ba8dc92dd9e9e32f37bacc
onyxlurae <onyxlbennett@gmail.com>
2026-06-23 18:35:03 -0400

    added mapping to holding, updated const/get/set to match

--- project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Models/InvestmentAccount.java
+++ project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Models/InvestmentAccount.java
@@ -1,6 +1,7 @@
 package com.skillstorm.Models;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
@@ -16,9 +17,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
-import com.skillstorm.Models.User;
 
 @Entity
 @Table(name = "investment_account")
@@ -39,75 +39,73 @@ public class InvestmentAccount {
     @Column(name = "date_opened", nullable = false)
     private Date dateOpened;
 
-
     @ManyToOne
-    @JsonIgnoreProperties(value = {"investmentAccounts"})
+    @JsonIgnoreProperties(value = { "investmentAccounts" })
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
+    @OneToMany(mappedBy = "security")
+    private List<Holding> holdings;
 
     public InvestmentAccount() {
     }
 
-
-    public InvestmentAccount(int id, String nickname, InvestmentType accountType, Date dateOpened, User user) {
+    public InvestmentAccount(int id, String nickname, InvestmentType accountType, Date dateOpened, User user,
+            List<Holding> holdings) {
         this.id = id;
         this.nickname = nickname;
         this.accountType = accountType;
         this.dateOpened = dateOpened;
         this.user = user;
+        this.holdings = holdings;
     }
-
 
     public int getId() {
         return id;
     }
 
-
     public void setId(int id) {
         this.id = id;
     }
-
 
     public String getNickname() {
         return nickname;
     }
 
-
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
-
 
     public InvestmentType getAccountType() {
         return accountType;
     }
 
-
     public void setAccountType(InvestmentType accountType) {
         this.accountType = accountType;
     }
-
 
     public Date getDateOpened() {
         return dateOpened;
     }
 
-
     public void setDateOpened(Date dateOpened) {
         this.dateOpened = dateOpened;
     }
-
 
     public User getUser() {
         return user;
     }
 
-
     public void setUser(User user) {
         this.user = user;
     }
 
-    
-    
+    public List<Holding> getHoldings() {
+        return holdings;
+    }
+
+    public void setHoldings(List<Holding> holdings) {
+        this.holdings = holdings;
+    }
+
 }
