2685cdbe0aee32c54127aac891acb67607e4e03b
Gina Peng <ginapeng2002@gmail.com>
2026-06-23 15:19:33 -0500

    feat: user service (no login/logout yet)

--- project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Controllers/UserController.java
+++ project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Controllers/UserController.java
@@ -1,5 +1,14 @@
 package com.skillstorm.Controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/users")
+@CrossOrigin(origins = "http://127.0.0.1:5500") // TODO: CHANGE THIS
 public class UserController {
+
+    
 
 }
--- project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Services/UserService.java
+++ project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Services/UserService.java
@@ -1,8 +1,54 @@
 package com.skillstorm.Services;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import com.skillstorm.DTOs.UserDto;
+import com.skillstorm.Models.User;
+import com.skillstorm.Repositories.UserRepo;
 
 @Service
 public class UserService {
+
+    private final UserRepo repo;
+
+    public UserService(UserRepo repo) {
+        this.repo = repo;
+    }
+
+    // REGISTRATION
+
+    ResponseEntity<User> registerUser(UserDto dto) {
+        return ResponseEntity.status(201).body(
+            repo.save(new User(0, dto.username(), dto.email(), dto.passwordHash(), dto.investmentAccounts()))
+        );
+    }
+
+    // VIEW PROFILE
+    ResponseEntity<User> viewProfile(int id) {
+        Optional<User> userOptional = repo.findById(id);
+        if (userOptional.isPresent()) {
+            return ResponseEntity.ok(userOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    // EDIT PROFILE
+
+    ResponseEntity<User> editProfile(int id, UserDto dto) {
+        if (repo.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.OK).body(
+                repo.save(new User(id, dto.username(), dto.email(), dto.passwordHash(), dto.investmentAccounts()))
+            );
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
+    // LOGIN (TODO)
+    // LOGOUT (TODO)
+    // DATA SCOPING?
     
 }
