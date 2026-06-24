879fe863ce47ed23ce8066f10b90f0b6ab60ce61
onyxlurae <onyxlbennett@gmail.com>
2026-06-24 15:28:15 -0400

    feat: added JsonValue

--- project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Models/SectorType.java
+++ project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Models/SectorType.java
@@ -1,5 +1,21 @@
 package com.skillstorm.Models;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum SectorType {
-    STOCK, ETF, MUTUAL_FUND, BOND
+    STOCK("Stock"),
+    ETF("ETF"),
+    MUTUAL_FUND("Mutual Fund"),
+    BOND("Bond");
+
+    private String name;
+
+    SectorType(String name) {
+        this.name = name;
+    }
+
+    @JsonValue
+    public String getName() {
+        return name;
+    }
 }
--- project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Models/SecurityType.java
+++ project2_personal_portfolio_tracker/src/main/java/com/skillstorm/Models/SecurityType.java
@@ -1,5 +1,26 @@
 package com.skillstorm.Models;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum SecurityType {
-    TECHNOLOGY, HEALTHCARE, FINANCIALS, CONSUMER, ENERGY, INDUSTRIALS, UTILITIES, REAL_ESTATE
+    TECHNOLOGY("Technology"),
+    HEALTHCARE("Healthcare"),
+    FINANCIALS("Financials"),
+    CONSUMER("Consumer"),
+    ENERGY("Energy"),
+    INDUSTRIALS("Industrials"),
+    UTILITIES("Utilities"),
+    REAL_ESTATE("Real Estate");
+
+    private String name;
+
+    SecurityType(String name) {
+        this.name = name;
+    }
+
+    @JsonValue
+    public String getName() {
+        return name;
+    }
+
 }
