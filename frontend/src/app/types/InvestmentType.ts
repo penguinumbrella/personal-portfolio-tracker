dbacbbff4cbf1f3625ffbc8e56077934f1ca67fc
Gina Peng <ginapeng2002@gmail.com>
2026-06-30 12:24:39 -0500

    feat: added enums for investmenttype

--- frontend/src/app/types/InvestmentType.ts
+++ frontend/src/app/types/InvestmentType.ts
@@ -1,3 +1,7 @@
 export enum InvestmentType {
-    
+    BROKERAGE = "Brokerage",
+    TRADITIONAL_IRA = "Traditional IRA",
+    ROTH_IRA = "Roth IRA",
+    K401 = "401(k)",
+    HSA = "ThrHSAiller",
 }
