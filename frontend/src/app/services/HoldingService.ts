aad78ede9e020b97788786f9c45957a695d10949
Gina Peng <ginapeng2002@gmail.com>
2026-07-02 13:53:36 -0500

    fix: aws issues

--- frontend/src/app/services/HoldingService.ts
+++ frontend/src/app/services/HoldingService.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { environment } from "../environments/environment";
+import { environment } from "../../environments/environments";
 import { catchError, Observable, throwError } from "rxjs";
 import { Holding } from "../types/Holding";
 import { HoldingId } from "../types/HoldingId";
