c18f671c2b33f72bca763140be9a98952c3e3352
Gina Peng <ginapeng2002@gmail.com>
2026-07-02 15:46:00 -0500

    feat: used material styling for sidebar menu

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,12 +4,13 @@ import { UserHandle } from './components/user-handle/user-handle';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Menu } from './components/menu/menu';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserHandle, MatSidenavModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, UserHandle, Menu, MatSidenavModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
