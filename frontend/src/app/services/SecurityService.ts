e8d5c7df41fc863c85331c86a1af59820f030042
onyxlurae <onyxlbennett@gmail.com>
2026-07-02 14:53:00 -0400

    fix env frontend folder loc

--- frontend/src/app/services/SecurityService.ts
+++ frontend/src/app/services/SecurityService.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { environment } from "../environments/environment";
+import { environment } from "../../environments/environments";
 import { catchError, Observable, throwError } from "rxjs";
 import { Security } from "../types/Security";
 
