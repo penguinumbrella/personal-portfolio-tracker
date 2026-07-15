f38830a91eb574d5247ae20f9368ef5ae4dd2e1c
Gina Peng <ginapeng2002@gmail.com>
2026-07-15 13:33:49 -0500

    feat: dark mode/light mode styling

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './services/ThemeService';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  private themeService = inject(ThemeService);
+}
