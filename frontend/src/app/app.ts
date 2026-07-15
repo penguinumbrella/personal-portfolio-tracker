6186d98d8fd5982f82c4cbce20ba7159ce423678
Gina Peng <ginapeng2002@gmail.com>
2026-07-15 13:36:13 -0500

    fix: merge issue

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,7 @@
 import { Component, inject } from '@angular/core';
-<<<<<<< HEAD
-import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './services/ThemeService';
-=======
 import { RouterOutlet, Router } from '@angular/router';
 import { AuthService } from './services/AuthService';
->>>>>>> 8bcb5fb87a75418c9f11476365922f80ef3e3192
 
 @Component({
   selector: 'app-root',
